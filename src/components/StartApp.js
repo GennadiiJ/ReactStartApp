a5f74e86d819c4c389ddc764d6ec594a76327742
GennadiiJ <zaporozhets@email.cz>
2019-03-13 15:34:40 +0100

    ex - next - jsx to js params

--- src/components/StartApp.js
+++ src/components/StartApp.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 function StartApp () {
+    let dayTime;
+    const hours = new Date().getHours();
+    const day = new Date().getDay();
+    if (hours < 12)
+        dayTime = "morning";
+    else if (hours >= 12 && hours < 18)
+        dayTime = "afternoon";
+    else dayTime = "night";
     return (
-        <main className="navBar">
-            This is header.
-        </main>
+        <div>
+            <h1>It is {dayTime} now</h1>
+            <h3>Another way to treat params {`${dayTime} "and" ${day}`}</h3>
+        </div>
     )
 }
 
